4986ea51f2591189fe74bd168dfac6e88c3914d6
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-31 15:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const width = document.querySelector("#widthValue");
+const height = document.querySelector("#heightValue");
+
+window.addEventListener("resize",()=>{
+   width.innerText = window.innerWidth;
+   height.innerText = window.innerHeight;
+})
